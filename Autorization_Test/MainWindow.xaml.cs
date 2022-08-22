e55f4c1773d991547f4c766114f7f53c599fe2b7
EvanPlay <99335865+EvanPlay@users.noreply.github.com>
2022-08-22 11:04:01 +0300

    Добавьте файлы проекта.

---
+++ Autorization_Test/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Autorization_Test
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+      
+
+        private void Autoriz_Click(object sender, RoutedEventArgs e)
+        {
+            if (tblogin.Text.Length > 0)
+            {
+                if (tbpassword.Text.Length > 0)
+                {
+                    if(tbpassword.Text == "101" && tblogin.Text == "101")
+                    {
+                        MessageBox.Show("Вход выполнен");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Неверен логин или пароль.");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Ведите пароль.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Ведите Логин.");
+            }
+        }
+    }
+}
+//login = 101
+//password = 101
